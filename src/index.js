62a1fb3386b9fb0dc673c5c4a97d1af257fda445
Zakui <zakuijules@gmail.com>
2019-02-14 13:20:00 +0000

    nouveau texte

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './style/main.less';
 
 class Acceuil extends React.Component {
     render () {
-        return <h1 className="header">Bonjour react app </h1>;
+        return <h1 className="header">Bonjour react app avec un nouveau text </h1>;
     }
 }
 
